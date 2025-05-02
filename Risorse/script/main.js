f4a011032dc08d6c80027ef0acc06e3887d44ca7
Dave02233 <davidecannistraro02@gmail.com>
2025-05-02 16:23:41 +0200

    Side Nav

--- Risorse/script/main.js
+++ Risorse/script/main.js
@@ -1,24 +1,63 @@
+let sideNavClosedByUser = false;
+
 let map = document.getElementById("mappa");
-let button = document.getElementById("showMap");
+let mapButton = document.getElementById("showMap");
 
 function reverseMapDisplay() {
+
     if (map.classList.contains("hidden") || !map.classList.contains("visible")) {
         
         map.classList.add("visible");
         map.classList.remove("hidden");
         console.log(map.classList);
 
-        button.innerHTML = "-";
+        mapButton.innerHTML = "-";
 
         setTimeout(() => map.scrollIntoView({ behavior: "smooth" }), 400); // Animazione conclusa a 500ms
 
     } else {
         map.classList.remove("visible");
         map.classList.add("hidden");
-        button.innerHTML = "+";
+        mapButton.innerHTML = "+";
     }
 }
 
+function toggleSideNav() {
 
 
-button.addEventListener("click", reverseMapDisplay);
+    
+    let scrollPercent = (window.scrollY / document.documentElement.scrollHeight) * 100;
+
+    let sideNav = document.querySelector(".side-nav");
+    
+    if (scrollPercent >= 3 && !sideNavClosedByUser) {
+        if (sideNav.classList.contains("hidden")) {
+            sideNav.classList.add("visible");
+            sideNav.classList.remove("hidden");
+        }
+    } else {
+        if (sideNav.classList.contains("visible")) {
+            sideNav.classList.add("hidden");
+            sideNav.classList.remove("visible");
+        }
+    }
+
+    if(scrollPercent < 5) {
+        sideNavClosedByUser = false;
+    }   
+    
+}
+
+function removeSideNav() {
+    let sideNav = document.querySelector(".side-nav");
+
+    sideNav.classList.add("hidden");
+    sideNav.classList.remove("visible");
+    
+    sideNavClosedByUser = true;
+}
+
+
+mapButton.addEventListener("click", reverseMapDisplay);
+
+window.addEventListener("scroll", toggleSideNav);
